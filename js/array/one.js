6b5dcbb50d81cb4deed9efe0357d38ef0ff1be30
KavithaSri1999 <skavithamani1999@gmail.com>
2021-10-26 11:15:58 +0530

    commit

--- js/array/one.js
+++ js/array/one.js
@@ -1,18 +1,20 @@
 //How to create array in JavaScript
 let size=[28,30,32,34,36];
 
-//How to access item from array in JavaScript
+/*How to access item from array in JavaScript
 console.log(size[0]);
 console.log(size[1]);
 console.log(size[2]);
 console.log(size[3]);
-console.log(size[4]);
+console.log(size[4]);*/
 
+let lastarr=size[size.length-1];
 //How to access last item from Array in JavaScript
-console.log(size[4]);
+console.log(lastarr);
 
-//How to access all items from Array using for loop
+/*How to access all items from Array using for loop
 for(i=0;i<=4;i=i+1)
 {
     console.log(size[i]);
 }
+*/
